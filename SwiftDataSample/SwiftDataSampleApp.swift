4b1f561ea527c57516802655eecf583457876f85
Deekshith Kasturi <dkasturi@chewy.com>
2025-04-18 23:53:08 +0530

    Initial Commit

---
+++ SwiftDataSample/SwiftDataSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSampleApp.swift
+//  SwiftDataSample
+//
+//  Created by Saikiran K on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
